3362b07416a61e692475ca9e9b5559d06e8f5966
Marlon Leite <marlonleite@gmail.com>
2020-03-11 09:15:00 -0300

    Add Raffling APIView

--- raffles/api/viewsets.py
+++ raffles/api/viewsets.py
@@ -20,4 +20,44 @@ class RaffleViewSet(ModelViewSet):
     lookup_field = "number"
 
 
+class RafflingAPIView(APIView):
+    """
+    RafflingAPIView Api
+    """
 
+    def get_object(self, number):
+        try:
+            return Raffle.objects.get(number=number)
+        except Raffle.DoesNotExist:
+            raise None
+
+    def get(self, request, format=None):
+
+        raffles = Raffle.objects.all()
+        items = [item.number for item in raffles]
+
+        try:
+
+            url = f"{settings.SWEEPSTAKE_URL}/api/"
+            response = requests.post(url, {"items": items})
+
+            if response.status_code == 200:
+                r = response.json()
+
+                raffle = self.get_object(r["item"])
+
+                if not raffle:
+                    return Response({
+                        "error": "Not found"
+                    }, status=status.HTTP_404_NOT_FOUND)
+
+                serializer = RaffleSerializer(instance=raffle)
+
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except (ConnectionError, ReadTimeout) as e:
+            return Response({"error": "Connection error"},
+                            status.HTTP_500_INTERNAL_SERVER_ERROR
+                            )
+
+        return Response({"error": "API Error."}, status=status.HTTP_400_BAD_REQUEST)
